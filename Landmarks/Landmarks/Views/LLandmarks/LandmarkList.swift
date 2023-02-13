c7164e152d4a1865a3d0b919b9174e60553421fb
kahbyte <kahbyte@gmail.com>
2023-02-13 08:43:04 -0300

    feat: added favorite mechanism

---
+++ Landmarks/Landmarks/Views/LLandmarks/LandmarkList.swift
@@ -0,0 +1,46 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Kauê Sales on 13/02/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    
+    @State private var showFavoritesOnly = false
+    @EnvironmentObject var modelData: ModelData
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            !showFavoritesOnly || landmark.isFavorite
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+                .navigationTitle("Landmarks")
+            }
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(ModelData())
+    }
+}
